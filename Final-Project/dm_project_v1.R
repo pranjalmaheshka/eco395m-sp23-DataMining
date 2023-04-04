3130be1d34fdef828313a7a0e90ff9c11650fe7d
Pranjal Maheshka <112534701+pranjalmaheshka@users.noreply.github.com>
2023-04-04 17:58:19 -0500

    Update dm_project_v1.R

--- Final-Project/dm_project_v1.R
+++ Final-Project/dm_project_v1.R
@@ -1,4 +1,34 @@
-load_data = read.csv('C:\\Users\\pranj\\Desktop\\Sp23 Data Mining - Scott\\DM_data\\ercot_load_data.csv')
+## Import libraries
+
+## Import data set 
+df2011 = read.csv('..\data\ED2011.csv')
+
+## Primary data cleaning
+# Make sure all columns can be read by R
+# Drop irrelevant columns
 
 
-## Test change for PCA branch
+## Data wrangling
+# Make one giant dataframe and ensure hospital codes dont get matched across years 
+
+
+## Data overview
+# Simple summary statistics or analysis as needed
+
+
+## Machine Learning: Process 1
+# PCA / other 
+
+
+## Machine Learning: Process 2
+# Random Forests / other
+
+
+## Plots
+# Partial dependence
+# Variable importance
+# Anything else 
+
+
+## Additional analysis 
+# TBD 
